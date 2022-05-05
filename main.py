d255478e600dc64c71f5d47ae3edc4451f43065b
100Ballov <100ballov@example.com>
2022-05-05 17:17:06 +0300

    Initinal commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import telebot
+
+bot = telebot.TeleBot('5396054230:AAFpE1oQKho9kNllbxkNobGhzz4aclELkAE')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == 'Привет как дела':
+        answer ='привет'
+        bot.send_message(message.from_user.id,answer)
+    elif message.text == '/help':
+        answer = 'Напишы "Привет"'
+        bot.send_message(message.from_user.id, answer)
+    else:
+         answer = 'ямпап "Привет"'
+         bot.send_message(message.from_user.id, answer)
+bot.polling(none_stop=True,interval= 0)
+def get_text_messages(message):
+    if message.text == 'Привет как дела':
+        answer ='привет хорошо'
+        bot.send_message(message.from_user.id,answer)
+    elif message.text == '/help':
+        answer = 'Напишы "Привет 1"'
+        bot.send_message(message.from_user.id, answer)
+    else:
+         answer = 'пока "Привет ми"'
+         bot.send_message(message.from_user.id, answer)
+
+
